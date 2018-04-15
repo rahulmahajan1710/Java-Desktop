cb8f4cf4320dfc018be829adb3dbc08e12d0d570
Rahul Mahajan <rahul_mahajan@apple.com>
2018-04-15 17:42:20 +0530

    Added icons

--- POC-1/src/window/Frame1.java
+++ POC-1/src/window/Frame1.java
@@ -2,19 +2,32 @@ package window;
 
 import java.awt.EventQueue;
 
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
 import java.awt.Component;
 import javax.swing.Box;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 public class Frame1 {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -44,26 +57,57 @@ public class Frame1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setForeground(Color.WHITE);
+		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GRAY);
+		panel.setBounds(174, 55, 270, 140);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel label = new JLabel("");
+		Image img = new ImageIcon(this.getClass().getResource("/user.png")).getImage();
+		label.setIcon(new ImageIcon(img));
+		label.setBounds(18, 28, 40, 40);
+		panel.add(label);
+		
+		textField = new JTextField();
+		textField.setBounds(72, 30, 192, 33);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setBounds(18, 80, 40, 40);
+		Image lockImg = new ImageIcon(this.getClass().getResource("/lock.png")).getImage();
+		label_1.setIcon(new ImageIcon(lockImg));
+		panel.add(label_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(72, 85, 192, 35);
+		panel.add(passwordField);
+		
+		JLabel lblNewLabel = new JLabel("User Login");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(249, 27, 100, 16);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Tapti POS");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("American Typewriter", Font.BOLD, 24));
+		lblNewLabel_1.setForeground(Color.ORANGE);
+		lblNewLabel_1.setBounds(25, 101, 127, 29);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(260, 231, 117, 29);
+		frame.getContentPane().add(btnNewButton);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int result = JOptionPane.showConfirmDialog(null, "Hello World!!");
-				if(result == JOptionPane.YES_OPTION) {
-					System.out.println("YES");
-				}
-			}
-		});
-		btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
-		frame.getContentPane().add(btnNewButton, BorderLayout.CENTER);
-		
-		Component verticalStrut = Box.createVerticalStrut(20);
-		frame.getContentPane().add(verticalStrut, BorderLayout.SOUTH);
-		
-		Component verticalStrut_1 = Box.createVerticalStrut(231);
-		frame.getContentPane().add(verticalStrut_1, BorderLayout.NORTH);
+		frame.setUndecorated(true);
+		frame.setLocationRelativeTo(null);
 	}
-
 }
